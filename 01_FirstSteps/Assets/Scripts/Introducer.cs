7622022ff65385fe63e54e151ab8626dc4f06e67
Johnathon Sullinger <sully@johnathons-mbp.lan>
2019-12-31 23:39:12 -0700

    Adding First Section to the training as a project

---
+++ 01_FirstSteps/Assets/Scripts/Introducer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Introducer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(10 / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
