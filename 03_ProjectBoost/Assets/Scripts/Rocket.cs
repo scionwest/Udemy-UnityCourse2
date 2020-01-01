c3e972734c201df873214a6d311d6144bada1194
Johnathon Sullinger <scionwest@gmail.com>
2020-01-01 00:53:51 -0700

    Initial hovering rocket.

---
+++ 03_ProjectBoost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating right");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating left");
+        }
+    }
+}
