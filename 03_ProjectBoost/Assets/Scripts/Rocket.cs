012189e216b18bbaa2e7ae496bc66c103895a71e
Johnathon Sullinger <scionwest@gmail.com>
2020-01-02 21:09:29 -0700

    Added sound and movement to the rocket.

--- 03_ProjectBoost/Assets/Scripts/Rocket.cs
+++ 03_ProjectBoost/Assets/Scripts/Rocket.cs
@@ -4,35 +4,58 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    private Rigidbody rigidBody;
+    [SerializeField] float thrustAmount = 200.0f;
+    [SerializeField] float rotationSpeed = 50.0f;
 
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+     
     // Start is called before the first frame update
     void Start()
     {
         this.rigidBody = this.GetComponent<Rigidbody>();
+        this.audioSource = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        ThrustShip();
+        RotateShip();
     }
 
-    private void ProcessInput()
+    private void ThrustShip()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            this.rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * this.thrustAmount * Time.deltaTime);
+
+            if (!this.audioSource.isPlaying)
+            {
+                this.audioSource.Play();
+            }
         }
+        else
+        {
+            this.audioSource.Stop();
+        }
+    }
+
+    private void RotateShip()
+    {
+        this.rigidBody.freezeRotation = true;
+        var rotationAmountForFrame = this.rotationSpeed * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating right");
+            transform.Rotate(Vector3.forward * rotationAmountForFrame);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating left");
+            transform.Rotate(-Vector3.forward * rotationAmountForFrame);
         }
+
+        this.rigidBody.freezeRotation = false; // Enable/Disable the physics based rotation applied by Rigid Body.
     }
 }
